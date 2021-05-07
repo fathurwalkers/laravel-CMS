d8dd64c82e39ada8e7dcf6ecc2abfac8234c8ee3
Fathur Walkers <fathurwalkers44@gmail.com>
2021-05-07 09:08:53 -0700

    update

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -7,5 +7,21 @@
 @endsection
 
 @section('main-content')
-    Dashboard Panel
+<div class="row">
+    <img src="#">
+    <div class="col-sm-2 col-md-2 col-lg-2">
+        Name <br>
+        Email <br>
+        Role <br> 
+        Status <br> 
+    </div>
+
+    <div class="col-sm-10 col-md-10 col-lg-10">
+        : Fathur Walkers
+        : fathurwalkers44@gmail.com 
+        : Administrator 
+        : Active 
+    </div>
+
+</div>
 @endsection
