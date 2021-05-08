1046fb0b4d2d98228e10ea22944e82bdd76d260a
Fathur Walkers <fathurwalkers44@gmail.com>
2021-05-08 10:52:00 -0700

    update

--- resources/views/layouts/admin-layout.blade.php
+++ resources/views/layouts/admin-layout.blade.php
@@ -125,11 +125,21 @@
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
+
               <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
                   <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
                   <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                </ul>
+              </li>
+              
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Posts</span></a>
+                <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="index-0.html">Posts</a></li>
+                  <li><a class="nav-link" href="index.html">Category</a></li>
+                  <li><a class="nav-link" href="index.html">Pages</a></li>
                 </ul>
               </li>
               <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
